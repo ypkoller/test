cf32cdc17b7aa75b488cf9c34a4e4be2caabfc0c
yves <yves@Harlekin-PC>
2014-10-16 21:57:38 +0200

    try

---
+++ test/src/test/java/ch/ysc/first/test/StartTest.java
@@ -0,0 +1,15 @@
+package ch.ysc.first.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class StartTest {
+	
+	@Test
+	public void testString() {
+		Start obj = new Start();
+		assertEquals("Gleicher Text","Hello World",obj.sayHelloWorld());
+	}
+
+}
