376f2b18be270ed372189962648dab418cab9b09
yves <yves@Harlekin-PC>
2014-10-17 11:14:08 +0200

    Configuration

--- test/src/main/java/ch/ysc/first/test/Start.java
+++ test/src/main/java/ch/ysc/first/test/Start.java
@@ -1,8 +1,13 @@
 package ch.ysc.first.test;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Start {
 	
+	private final static Logger s_logger = LogManager.getLogger();
 	public String sayHelloWorld(){
+		s_logger.info("Start Hello World");
 		return "Hello World";
 	}
 
